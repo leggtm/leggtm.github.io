a720f6722060c3e751aa091624505a45f59d8554
Chiara Adjoh-Davoh <118475244+leggtm@users.noreply.github.com>
2026-03-31 22:45:11 -0400

    created postcard landing page
    did a LOT to create postcard landing page with postcard image that is clickable.
    
    Signed-off-by: Chiara Adjoh-Davoh <118475244+leggtm@users.noreply.github.com>

---
+++ index.js
@@ -0,0 +1,8 @@
+
+const card = document.querySelector(".innerCard");
+const body = document.querySelector("body");
+
+card.addEventListener("click", function (e) {
+    card.classList.toggle('isFlipped');
+    body.classList.add('canScroll')
+});
