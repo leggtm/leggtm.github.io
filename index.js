4c7917ba63a079afedd25205b14ae1a2ba485e08
Chiara Adjoh-Davoh <118475244+leggtm@users.noreply.github.com>
2026-04-02 19:28:00 -0400

    fixed issues with the navbar that were causing it to squish on smaller sized screens
    still need to create a hamburger menu for smaller screens and mobile
    
    Signed-off-by: Chiara Adjoh-Davoh <118475244+leggtm@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+/* these are my scripts. javascript is the languge that i am
+    least familiar with out of the three, so im learning 
+    as i go. */
 
+// to toggle the card flipping animation
 const card = document.querySelector(".innerCard");
 const body = document.querySelector("body");
 const navbar = document.querySelector(".navbar");
@@ -12,10 +16,14 @@ card.addEventListener("click", function (e) {
     }, 180);
 });
 
-
+// the make the navbar only appear after a certain amount of scroll
 window.addEventListener("scroll", function (e){
     const scrollY = window.scrollY;
-    if (scrollY > 500){
+    const docHeight = document.documentElement.scrollHeight;
+    const winHeight = window.innerHeight;
+    const scrollPercent = (scrollY/ (docHeight - winHeight)) * 100;
+
+    if (pagecontents.classList.contains('beenFlipped') && scrollPercent > 25){
         navbar.classList.add('beenFlipped');
     }
 });
