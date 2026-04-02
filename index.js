0b84838ae7d1a53996da380fbc53512a65eabda5
Chiara Adjoh-Davoh <118475244+leggtm@users.noreply.github.com>
2026-04-01 20:57:53 -0400

    made major updates: navbar, sections, slight update to card
    still very barebones but the basic template has been set in stone
    
    Signed-off-by: Chiara Adjoh-Davoh <118475244+leggtm@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 
 const card = document.querySelector(".innerCard");
 const body = document.querySelector("body");
+const navbar = document.querySelector(".navbar");
+const pagecontents = document.querySelector(".page_contents");
 
 card.addEventListener("click", function (e) {
     card.classList.toggle('isFlipped');
+
+    setTimeout(() => {
+        pagecontents.classList.add('beenFlipped');
+    }, 180);
 });
+
+
+window.addEventListener("scroll", function (e){
+    const scrollY = window.scrollY;
+    if (scrollY > 500){
+        navbar.classList.add('beenFlipped');
+    }
+});
+
